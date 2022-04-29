cd84fbb5f7fe03eb10b30fa752a799a7a7aba8bc
vladpotapov <vladpotapov@yandex-team.ru>
2022-04-29 20:32:44 +0300

    vim: filter react.d.ts files from definition results

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,33 +1,48 @@
 local lsp_installer = require('nvim-lsp-installer')
-local lsp_config = require('lspconfig')
 local cmp_lsp = require('cmp_nvim_lsp')
+local utils = require('utils')
 
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
+local options = {
+    tsserver = {
+        on_attach = function(client)
+            client.resolved_capabilities.document_formatting = false
+            client.resolved_capabilities.document_range_formatting = false
+        end,
 
-    server:setup(opts)
-end)
+        capabilities = cmp_lsp.update_capabilities(
+            vim.lsp.protocol.make_client_capabilities()
+        ),
 
-lsp_config.tsserver.setup({
-    on_attach = function(client)
-        client.resolved_capabilities.document_formatting = false
-        client.resolved_capabilities.document_range_formatting = false
-    end,
+        handlers = {
+            ['textDocument/definition'] = function(err, result, method, ...)
+                if vim.tbl_islist(result) and #result > 1 then
+                    local filtered_result = utils.filter(result, utils.filterReactDTS)
+                    return vim.lsp.handlers['textDocument/definition'](err, filtered_result, method, ...)
+                end
 
-    capabilities = cmp_lsp.update_capabilities(
-        vim.lsp.protocol.make_client_capabilities()
-    ),
-})
+                vim.lsp.handlers['textDocument/definition'](err, result, method, ...)
+            end
+        },
+    },
 
--- lsp_config.sumneko_lua.setup({
---     settings = {
---         Lua = {
---             diagnostics = {
---                 globals = { 'vim' }
---             }
---         }
---     }
--- })
+    sumneko_lua = {
+        settings = {
+            Lua = {
+                diagnostics = {
+                    globals = { 'vim' }
+                }
+            }
+        }
+    }
+}
+
+lsp_installer.on_server_ready(
+    function(server)
+        local opts = options[server.name] or {}
+
+        server:setup(opts)
+    end
+)
 
 vim.diagnostic.config({
     virtual_text = false,
---
+++ lua/utils.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+function M.filter(arr, fn)
+    if type(arr) ~= "table" then
+        return arr
+    end
+
+    local filtered = {}
+
+    for k, v in pairs(arr) do
+        if fn(v, k, arr) then
+            table.insert(filtered, v)
+        end
+    end
+
+    return filtered
+end
+
+function M.filterReactDTS(value)
+    return string.match(value.uri, 'react/index.d.ts') == nil
+end
+
+return M
