5f780d6218a6a18707455c54cb8d5ae9e414d4db
bercly0b <vladpotapov@yandex-team.ru>
2022-05-16 10:32:21 +0300

    vim: packer (#9)

---
+++ lua/init-plugs.lua
@@ -0,0 +1,69 @@
+local packer = require('packer')
+
+return packer.startup(function(use)
+    use('wbthomason/packer.nvim')
+
+    use('windwp/nvim-autopairs')
+
+    use('christoomey/vim-tmux-navigator')
+
+    use({ 'tyru/open-browser.vim', opt = true, cmd = 'OpenGithubFile' })
+    use({ 'tyru/open-browser-github.vim', opt = true, cmd = 'OpenGithubFile' })
+
+    -- g/
+    use('google/vim-searchindex')
+
+    use('kyazdani42/nvim-web-devicons')
+
+    -- navigation
+    use('jlanzarotta/bufexplorer')
+    use('kyazdani42/nvim-tree.lua')
+
+    -- gS gJ
+    use('AndrewRadev/splitjoin.vim')
+
+    -- telescope
+    use('nvim-lua/popup.nvim')
+    use('nvim-lua/plenary.nvim')
+    use('nvim-telescope/telescope.nvim')
+    use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
+
+    -- theme
+    use('navarasu/onedark.nvim')
+
+    use('tpope/vim-commentary')
+    use('tpope/vim-sensible')
+    use('wellle/targets.vim')
+
+    -- change/edit brackets
+    use('tpope/vim-surround')
+
+    -- git
+    use('airblade/vim-gitgutter')
+    use('tpope/vim-fugitive')
+
+    -- status line
+    use('nvim-lualine/lualine.nvim')
+    use('bercly0b/lualine-lsp-progress')
+
+    -- syntax highligt
+    use({ 'wavded/vim-stylus', ft = { 'css', 'scss', 'less', 'sass' } })
+    use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
+
+    -- lsp
+    use('neovim/nvim-lspconfig')
+    use('williamboman/nvim-lsp-installer')
+    -- linters
+    use('jose-elias-alvarez/null-ls.nvim')
+
+    -- autocomplete
+    use('hrsh7th/nvim-cmp')
+    use('hrsh7th/cmp-nvim-lsp')
+    use('hrsh7th/cmp-buffer')
+    use('hrsh7th/cmp-cmdline')
+    use('onsails/lspkind-nvim') -- cmp sources icons
+
+    -- snippets
+    use('L3MON4D3/LuaSnip')
+    use('saadparwaiz1/cmp_luasnip')
+end)
