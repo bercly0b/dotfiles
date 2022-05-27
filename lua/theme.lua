02770798f565aadfadfd3032a9e2479043c96ebf
vladpotapov <vladpotapov@yandex-team.ru>
2022-05-27 16:06:22 +0300

    theme init to lua

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,12 +1,4 @@
-vim.cmd('colorscheme onedark')
-
-vim.o.background = 'dark'
-
-vim.env.NVIM_TUI_ENABLE_TRUE_COLOR=1
-
-vim.g.javascript_plugin_jsdoc = 1
-vim.g.javascript_plugin_ngdoc = 1
-vim.g.vim_jsx_pretty_colorful_config = 1
+require('onedark').load()
 
 local colors = require('onedark.colors')
 
