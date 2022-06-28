db420b1a3f15d0e280cf1ed2d39ed25900913cab
bercly0b <vvworkmail@yandex.ru>
2022-06-28 17:51:13 +0300

    main: install script

--- README.md
+++ README.md
@@ -2,17 +2,33 @@
 
 Configuration files for zsh, tmux and neovim
 
-## deps
+## Installation
+```bash
+sudo apt update && sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# next script will overwrite your zsh, neovim and tmux config files in home dir. be careful
+# todo exec script
+```
+
+## Manual installation
+
+First install dependencies
+
+### dependencies
+- [neovim](https://neovim.io/)
 - [packer.nvim](https://github.com/wbthomason/packer.nvim)
 - [tmux](https://github.com/tmux/tmux)
+- [tpm](https://github.com/tmux-plugins/tpm)
 - [zsh](https://ohmyz.sh/)
-- [iterm2](https://iterm2.com/)
-- [neovim](https://neovim.io/)
+- [zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md)
 - [fzf](https://github.com/junegunn/fzf)
 - [fd-find](https://github.com/sharkdp/fd)
 - [ripgrep](https://github.com/BurntSushi/ripgrep)
+- [iterm2](https://iterm2.com/)
 
-## install
+Next clone and init this repository
+
 ```bash
 mkdir ~/self
 git clone https://github.com/bercly0b/dotfiles.git ~/self/dotfiles
@@ -20,12 +36,9 @@ git clone https://github.com/bercly0b/dotfiles.git ~/self/dotfiles
 ln -s ~/self/dotfiles/lua ~/.config/nvim/lua
 ln -s ~/self/dotfiles/snippets ~/.config/nvim/snippets
 
-# next commands will overwrite your config files. be careful
+# next commands will overwrite your zsh, neovim and tmux config files in home dir. be careful
 echo "require('init')" > ~/.config/nvim/init.lua
 echo "source-file ~/self/dotfiles/.tmux.conf" > ~/.tmux.conf
 echo "source ~/self/dotfiles/.zshrc" > ~/.zshrc
 rm ~/.config/nvim/init.vim
 ```
-
-## TODO
-- install script
---
+++ init-env.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -e
+
+_print () {
+    if [[ $(command -v tput) ]]; then
+        echo "$(tput setaf 2)### $1$(tput sgr0)"
+    else
+        echo "### $1"
+    fi
+}
+
+_print "Updating packages information"
+sudo apt update
+
+_print "Installing dependencies"
+sudo apt install -y tmux fd-find ripgrep htop
+sudo ln -s $(which fdfind) /bin/fd
+
+# nvim
+_print "Installing Neovim"
+curl -LO -s https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb
+sudo apt install -y ./nvim-linux64.deb
+rm ./nvim-linux64.deb
+
+# packer
+_print "Installing Packer"
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# tmux tmp
+_print "Installing tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# zsh-syntax-highlighting
+_print "Installing zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git\
+    ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# fzf
+_print "Installing fzf"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
+
+# eslint_d
+if [[ $(command -v npm) ]]; then
+    _print "Installing esling_d"
+    sudo npm install -g eslint_d
+fi
+
+# set vim as git editor
+git config --global core.editor "vim"
+
+mkdir ~/self
+mkdir -p ~/.config/nvim
+
+_print "Applying configs"
+git clone https://github.com/bercly0b/dotfiles.git ~/self/dotfiles
+
+ln -s ~/self/dotfiles/lua ~/.config/nvim/lua
+ln -s ~/self/dotfiles/snippets ~/.config/nvim/snippets
+
+echo "require('init')" > ~/.config/nvim/init.lua
+echo "source-file ~/self/dotfiles/.tmux.conf" > ~/.tmux.conf
+echo "source ~/self/dotfiles/.zshrc" > ~/.zshrc
+
+_print "Done!"
