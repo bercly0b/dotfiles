dcd59256b400d2d2e3ade20f4f1f1e8f100a0803
Vlad Potapov <vvworkmail@yandex.ru>
2022-11-23 16:23:24 +0100

    all: light theme (#13)
    * all: tmux and vim theme
    
    * iterm2: update colorscheme
    
    * vim: github lisht theme
    
    * tmux: github theme
    
    * tmux: add widghet to github theme
    
    * tmux: remove extra themes
    
    * vim: change theme to default

--- lua/init-plugs.lua
+++ lua/init-plugs.lua
@@ -33,7 +33,8 @@ return packer.startup(function(use)
     use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
 
     -- theme
-    use('navarasu/onedark.nvim')
+    -- use('navarasu/onedark.nvim')
+    use('projekt0n/github-nvim-theme')
 
     use('tpope/vim-commentary') --
     use('tpope/vim-sensible') --
--- lua/theme.lua
+++ lua/theme.lua
@@ -1,6 +1,11 @@
-require('onedark').load()
+-- require('onedark').load()
 
-local colors = require('onedark.colors')
+-- local colors = require('onedark.colors')
 
-vim.cmd('highlight NvimTreeNormal guibg='..colors.bg0)
-vim.cmd('highlight NvimTreeEndOfBuffer guibg='..colors.bg0)
+-- vim.cmd('highlight NvimTreeNormal guibg='..colors.bg0)
+-- vim.cmd('highlight NvimTreeEndOfBuffer guibg='..colors.bg0)
+
+require('github-theme').setup({
+    theme_style = 'light',
+    dark_float = true,
+})
