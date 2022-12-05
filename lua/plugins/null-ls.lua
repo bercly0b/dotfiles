dd701923d53c41f2ca539b6c275cec3d642e91d2
becly0b <vvworkmail@yandex.ru>
2022-12-05 17:10:55 +0300

    vim: update null-ls config for yarn pnp

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,9 +1,23 @@
 local null_ls = require('null-ls')
+local command_resolver = require('null-ls.helpers.command_resolver')
+
+local is_pnp = vim.fn.findfile('.pnp.js', '.')
+local eslint_diagnostics = null_ls.builtins.diagnostics.eslint_d
+local eslint_formatting = null_ls.builtins.formatting.eslint_d
+
+if (is_pnp) then
+    eslint_diagnostics = null_ls.builtins.diagnostics.eslint.with({
+        dynamic_command = command_resolver.from_yarn_pnp()
+    })
+    eslint_formatting = null_ls.builtins.formatting.eslint.with({
+        dynamic_command = command_resolver.from_yarn_pnp()
+    })
+end
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.eslint_d,
+        eslint_diagnostics,
+        eslint_formatting,
 
         null_ls.builtins.diagnostics.stylelint,
         null_ls.builtins.formatting.stylelint,
