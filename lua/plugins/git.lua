872f54b5e39b738545ea45194bee595c890ad60a
bercly0b <vvworkmail@yandex.ru>
2024-08-10 13:30:37 +0300

    vim: update gitsign config

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,11 +2,11 @@ local gitsigns = require('gitsigns')
 
 gitsigns.setup({
     signs = {
-        add = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-        change = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        delete = { hl = 'GitSignsDelete', text = '契', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        topdelete = { hl = 'GitSignsDelete', text = '契', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        changedelete = { hl = "GitSignsChange", text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+        add = { text = '▎' },
+        change = { text = '▎' },
+        delete = { text = '契' },
+        topdelete = { text = '契' },
+        changedelete = { text = '▎' },
     },
     signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
     numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -36,8 +36,5 @@ gitsigns.setup({
         relative = 'cursor',
         row = 0,
         col = 1,
-    },
-    yadm = {
-        enable = false,
     },
 })
