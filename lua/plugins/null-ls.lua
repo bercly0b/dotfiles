dbaaac29ac126386003b37ad47d50b5be7f0b63a
bercly0b <vvworkmail@yandex.ru>
2025-09-26 15:29:24 +0400

    vim: use ruff for python diagnostic

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,24 +1,19 @@
 local null_ls = require('null-ls')
 local mason_null_ls = require('mason-null-ls')
 
-local eslint_diagnostics = require('none-ls.diagnostics.eslint_d')
-local eslint_formatting = require('none-ls.formatting.eslint_d')
-
 mason_null_ls.setup({
-    ensure_installed = { 'eslint_d', 'ruff', 'black' }
+    ensure_installed = { 'eslint_d', 'ruff' }
 })
 
 null_ls.setup({
     sources = {
-        eslint_diagnostics,
-        eslint_formatting,
+        require('none-ls.diagnostics.eslint_d'),
+        require('none-ls.formatting.eslint_d'),
 
         null_ls.builtins.diagnostics.stylelint,
         null_ls.builtins.formatting.stylelint,
 
-        -- require('none-ls-extras.nvim.diagnostics.ruff'),
-        -- null_ls.builtins.formatting.black.with({
-        --     prefer_local = 'python_venv/bin',
-        -- }),
+        require('none-ls.diagnostics.ruff'),
+        require('none-ls.formatting.ruff'),
     },
 })
