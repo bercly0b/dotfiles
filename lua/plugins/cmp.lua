4afc3c6f39e7bf30f06d073293844406b07667be
bercly0b <vvworkmail@yandex.ru>
2025-09-27 21:43:40 +0400

    vim: remove custom func from cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,18 +2,11 @@ vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
 local cmp = require('blink.cmp')
 
-local function smart_select_next()
-    if cmp.is_visible() then
-        cmp.select_next()
-    else
-        cmp.show()
-    end
-end
 
 cmp.setup({
     keymap = {
         preset = 'default',
-        ['<C-j>'] = { smart_select_next, 'fallback' },
+        ['<C-j>'] = { 'show', 'select_next', 'fallback' },
         ['<C-k>'] = { 'select_prev', 'fallback' },
         ['<C-[>'] = { 'hide', 'fallback' },
         ['<CR>'] = { 'accept', 'fallback' },
@@ -63,7 +56,7 @@ cmp.setup({
             }
         },
         keymap = {
-            ['<C-j>'] = { smart_select_next },
+            ['<C-j>'] = { 'show', 'select_next' },
             ['<C-k>'] = { 'select_prev' },
             ['<C-[>'] = { 'hide' },
             ['<CR>'] = { 'accept_and_enter', 'fallback' },
